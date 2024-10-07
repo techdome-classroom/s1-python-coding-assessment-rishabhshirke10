3b934650eb1c69552c20569f84c59535f831fd0d
Rishabh Shirke <shirkerishabh10@gmail.com>
2024-10-07 12:31:31 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,10 +6,8 @@ def decode_message( s: str, p: str) -> bool:
     dp[0] = True  # Empty pattern matches empty string
     
     for i in range(1, m + 1):
-        # Previous row reference
         prev_dp = dp[:]
         
-        # Reset dp[0] for the current row if the pattern character is '*'
         dp[0] = dp[0] and p[i - 1] == '*'
         
         for j in range(1, n + 1):
