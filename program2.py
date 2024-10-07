dd95602fd41f0fdf93b9a0d9198f3202c53b4f82
Rishabh Shirke <shirkerishabh10@gmail.com>
2024-10-07 12:28:05 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,20 +1,5 @@
-class Solution:
-    def decode_message(self, s: str, p: str) -> bool:
-        m, n = len(p), len(s)
-        # Initialize a 1D DP array
-        dp = [False] * (n + 1)
-        dp[0] = True
+def decode_message( s: str, p: str) -> bool:
 
-        for i in range(1, m + 1):
-            prev_dp = dp[:]
-            dp[0] = dp[0] and p[i - 1] == "*"
-
-            for j in range(1, n + 1):
-                if p[i - 1] == "*":
-                    dp[j] = dp[j - 1] or prev_dp[j]
-                elif p[i - 1] == "?" or p[i - 1] == s[j - 1]:
-                    dp[j] = prev_dp[j - 1]
-                else:
-                    dp[j] = False
-
-        return dp[n]
+# write your code here
+  
+        return False
